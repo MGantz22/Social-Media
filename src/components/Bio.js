77776ead26d6ad353d5bcbe2b57e4e14b62cfbc1
Mitchell Gantz <mgantz22@icloud.com>
2023-04-17 19:40:48 -0700

    Update Bio

---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bio(){
+  return (
+    <p>I was born in 1971 in Pretoria, South Africa, oldest of three siblings. My father was a South African engineer, and my mother was a Canadian model and nutritionist.
+
+    After my parents divorced in 1980, I lived primarily with my father. Now, I think my father is “a terrible human being...almost every evil thing you could possibly think of, he has done.”</p>
+  );
+}
+
+export default Bio;
