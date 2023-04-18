ea9896ebedbb80f6c25943c1fd8b0c3ed9aa26f8
Mitchell Gantz <mgantz22@icloud.com>
2023-04-17 19:41:10 -0700

    Add friends

---
+++ src/components/Friends.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import FriendsInfo from "./FriendsInfo";
+
+
+function Friends(){
+  return (
+    <React.Fragment>
+      <Container className="mt=5">
+      <h3>People you may know:</h3>
+      <FriendsInfo />
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Friends;
